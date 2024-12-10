f0aa91f60c00c95a7d333bfc6e51c732f34fe75c
Grant Growcott <grantgrowcott56@gmail.com>
2024-12-10 10:22:36 -0500

    Added the favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import NavBar from "@/components/NavBar";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <NavBar />
       <Component {...pageProps} />
     </>
