fb8ac4d3a5d7ae95758c9365755c46b03271eb57
Grant Growcott <grantgrowcott56@gmail.com>
2024-12-20 19:35:38 -0500

    Changed to classes for styling

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,38 +5,21 @@ import logo from "../../public/logo-text.png";
 
 const Custom404: React.FC = () => {
   return (
-      <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", backgroundColor: "white", height: "100vh" }}>
-        <div style={{ maxWidth: "60%", margin: "0 auto", paddingBottom: "2em" }}>
-          <h1 style={{ fontSize: "48px", marginBottom: "1em" }}>Oops! We lost this page! 404 Error</h1>
-          <p style={{ lineHeight: "1.5", color: "black"}}>We searched high and low and couldn&apos;t find a better place for you to go.</p>
-          <p style={{ lineHeight: "1.5", color: "black" }}>Maybe try checking the URL or going back to our</p>
-          <button
-            style={{
-              backgroundColor: "#2bbb35",
-              padding: "1em 1.7em",
-              borderRadius: "30px",
-              border: "none",
-              cursor: "pointer",
-              borderWidth: "0.5mm",
-              borderStyle: "solid",
-              borderColor: "black",
-              textDecoration: "none",
-            }}
-          >
-            <Link href="/">
-              <a style={{ color: "#000000", textDecoration: "none", fontWeight: "bold" }}>Home Page</a>
-            </Link>
-          </button>
-        </div>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <Image
-            src={logo}
-            alt="Ninja confused"
-            style={{ width: "190px", height: "70px" }}
-          />
-        </div>
+    <div className="error__container">
+      <div className="error__container__inner">
+        <h1>Oops! We lost this page! 404 Error</h1>
+        <p>We searched high and low and couldn&apos;t find a better place for you to go.</p>
+        <p>Maybe try checking the URL or going back to our</p>
+        <button>
+          <Link href="/">
+            <a>Home Page</a>
+          </Link>
+        </button>
       </div>
-    
+      <div className="logo__container">
+        <Image src={logo} alt="Ninja confused" style={{ width: "190px", height: "70px" }} />
+      </div>
+    </div>
   );
 };
 
