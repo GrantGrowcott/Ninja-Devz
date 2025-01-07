ea6299693c3a62688d62a1679ff4bf540d2da363
Grant Growcott <grantgrowcott56@gmail.com>
2025-01-07 17:51:19 -0500

    More stuff

--- src/components/FAQSection.tsx
+++ src/components/FAQSection.tsx
@@ -12,33 +12,24 @@ const FAQSection = () => {
     setMovies([...movies]);
   };
 
-
   return (
     <div>
       {faqs.map((faqs, index) => (
         <div key={index}>
-          <div className="category__info__container">
-            <div className="movie__name">
-              <h3>{faqs.question}</h3>
-            </div>
-            <button
-              className="category__info__button"
-              onClick={() => toggleDropdown(index)} 
-            >
+          <button className="category__info__container" onClick={() => toggleDropdown(index)}>
+            <h3>{faqs.question}</h3>
+            <span className="category__info__button">
               <h3>+</h3>
-            </button>
-          </div>
+            </span>
+          </button>
+
           {/* Only render the dropdown menu if this movie's index is in dropdownRefs */}
           {dropdownRefs.current[index] && (
             <>
-              <div className="dropdown__menu">
-                <div className="dropdown__menu__container"></div>
-              </div>
+              <div className="dropdown__menu"></div>
               <div className="synopsis__container">
                 <h3>{faqs.answer} </h3>
               </div>
-              <div className="movie__stats"></div>
-              <div className="modify__container"></div>
             </>
           )}
         </div>
